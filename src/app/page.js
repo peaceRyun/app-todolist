a59c17a2e32cea9255e6b4bb950b41bd29d9d071
Yoongu Kwon <dbsrn1110@gmail.com>
2024-11-25 17:40:15 +0900

    mui 설치 및 메인 tod0화면에 버튼 생성

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,15 @@
 
 import Todo from '@/components/Todo';
 import { useTheme } from '@/contexts/ThemeContext';
+import { Button } from '@mui/material';
 
 export default function Home() {
     const theme = useTheme();
     return (
         <div className={(theme.background, theme.text)}>
+            <Button variant='contained' color='primary'>
+                Click Me
+            </Button>
             <Todo />
         </div>
     );
